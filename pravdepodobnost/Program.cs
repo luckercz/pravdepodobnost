5b723f318099a30c622383a71fd137018f0c8d2d
luckercz <lukas.andrysek@gmail.com>
2024-01-15 13:19:20 +0100

    Completly reworked

--- pravdepodobnost/Program.cs
+++ pravdepodobnost/Program.cs
@@ -4,8 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ahoj svete");
-            Console.WriteLine("ahoj lide");
+            Random rnd = new Random();
+            int correct = 0;
+            int series = 10
+                00;
+
+            for (int serieNumber = 0; serieNumber < series; serieNumber++)
+            {
+                int inRow = 0;
+                int lastFlip = 0;
+                for (int flipNumber = 0; flipNumber < 100; flipNumber++)
+                {
+                    int flip = rnd.Next(2);
+                    if (flip == lastFlip) inRow++;
+                    else
+                    {
+                        inRow = 0;
+                        lastFlip = 1 - lastFlip;
+                    }
+
+                    if (inRow == 7)
+                    {
+                        correct++;
+                        break;
+                    }
+                }
+            }
+            double propability = ((double)correct / series)*100;
+            Console.WriteLine(propability);
         }
     }
 }
