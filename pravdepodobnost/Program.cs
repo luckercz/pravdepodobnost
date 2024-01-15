4bc9951f88a2469da6428a99aae3e8d97aa24b22
luckercz <lukas.andrysek@gmail.com>
2024-01-15 14:21:54 +0100

    added CWs

--- pravdepodobnost/Program.cs
+++ pravdepodobnost/Program.cs
@@ -5,22 +5,24 @@
         static void Main(string[] args)
         {
             Random rnd = new Random();
+
+            Console.WriteLine("Napis pocet serii:");
+            int series = 10000;
+            Console.WriteLine("Napis kolikrat za sebou ma padnout:");
             int correct = 0;
-            int series = 10
-                00;
 
             for (int serieNumber = 0; serieNumber < series; serieNumber++)
             {
                 int inRow = 0;
-                int lastFlip = 0;
+                int lastFlip = -1;
                 for (int flipNumber = 0; flipNumber < 100; flipNumber++)
                 {
                     int flip = rnd.Next(2);
                     if (flip == lastFlip) inRow++;
                     else
                     {
-                        inRow = 0;
-                        lastFlip = 1 - lastFlip;
+                        inRow = 1;
+                        lastFlip = flip;
                     }
 
                     if (inRow == 7)
@@ -31,7 +33,7 @@
                 }
             }
             double propability = ((double)correct / series)*100;
-            Console.WriteLine(propability);
+            Console.WriteLine(propability + "%");
         }
     }
 }
